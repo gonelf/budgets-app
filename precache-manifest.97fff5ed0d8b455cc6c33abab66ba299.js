c7c10f8a894de5fc26034b1b0758164355204e9f
Gonçalo Henriques <gonelf@gmail.com>
2019-11-26 20:28:58 +0000

    new app with login

---
+++ precache-manifest.97fff5ed0d8b455cc6c33abab66ba299.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = [
+  {
+    "revision": "9b572a35580a888e22ae",
+    "url": "/static/js/app.403b9071.chunk.js"
+  },
+  {
+    "revision": "01748bfc9c51c385bd7e",
+    "url": "/static/js/runtime~app.306130d3.js"
+  },
+  {
+    "revision": "82f7d61edc508c53a8b0",
+    "url": "/static/js/2.4fc7d10c.chunk.js"
+  },
+  {
+    "revision": "004c2bbb035d8d06bb830efc4673c886",
+    "url": "/static/media/star.004c2bbb.png"
+  },
+  {
+    "revision": "28ea5d6a05ad28a7a253346e5c5e8964",
+    "url": "/static/media/flame.28ea5d6a.png"
+  },
+  {
+    "revision": "2327736b3ea09c41abfb69be1221f430",
+    "url": "/static/media/heart.2327736b.png"
+  },
+  {
+    "revision": "51671417ef20e0bbc32f0a2bc6edfa95",
+    "url": "/static/media/rocket.51671417.png"
+  },
+  {
+    "revision": "a7b9dc9de5f8f1fb1afbef917619a5ac",
+    "url": "/static/media/bell.a7b9dc9d.png"
+  },
+  {
+    "revision": "b2e0fc821c6886fb3940f85a3320003e",
+    "url": "/./fonts/Ionicons.ttf"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/./fonts/MaterialIcons.ttf"
+  },
+  {
+    "revision": "5a293a273bee8d740a045d9922b9a9ae",
+    "url": "/./fonts/MaterialCommunityIcons.ttf"
+  },
+  {
+    "revision": "e20945d7c929279ef7a6f1db184a4470",
+    "url": "/./fonts/Foundation.ttf"
+  },
+  {
+    "revision": "744ce60078c17d86006dd0edabcd59a7",
+    "url": "/./fonts/Entypo.ttf"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/./fonts/FontAwesome.ttf"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/./fonts/SimpleLineIcons.ttf"
+  },
+  {
+    "revision": "6beba7e6834963f7f171d3bdd075c915",
+    "url": "/./fonts/Feather.ttf"
+  },
+  {
+    "revision": "3a2ba31570920eeb9b1d217cabe58315",
+    "url": "/./fonts/AntDesign.ttf"
+  },
+  {
+    "revision": "f82b34f900882c5120a1bfbf6df22a27",
+    "url": "/static/media/icon.f82b34f9.png"
+  },
+  {
+    "revision": "6165c9d7a2e729ba57b23dd93add5366",
+    "url": "/static/media/back-icon-mask.6165c9d7.png"
+  },
+  {
+    "revision": "c6aef942e3668158ec29d4adcb2e768f",
+    "url": "/./fonts/FontAwesome5_Brands.ttf"
+  },
+  {
+    "revision": "872545dde71de3842234bf6afe80c4cb",
+    "url": "/./fonts/FontAwesome5_Solid.ttf"
+  },
+  {
+    "revision": "d0c694b562b2208635f250762cd7fc79",
+    "url": "/serve.json"
+  },
+  {
+    "revision": "7a7bc7ead25db795e58b336f04d2624c",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "5e695e96a003a79f7f97060bf49409a9",
+    "url": "/expo-service-worker.js"
+  },
+  {
+    "revision": "020fd499a2d2bc45c4c3b6021e472b43",
+    "url": "/register-service-worker.js"
+  },
+  {
+    "revision": "868df6b52a4a7f4dee4ccf1dc7becc98",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "e6d1b63765ccecc78ab541e397665bc2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "181854557f6f9731fa67765d6e29fb4f",
+    "url": "/static/js/runtime~app.306130d3.js.gz"
+  },
+  {
+    "revision": "67b1235b79b2a3c7fc875cf686edef82",
+    "url": "/expo-service-worker.js.gz"
+  },
+  {
+    "revision": "bda51777107be3be17c45a42dc59fb0e",
+    "url": "/static/js/app.403b9071.chunk.js.gz"
+  },
+  {
+    "revision": "2321af8625a080bcfc6caec2c7c27c1a",
+    "url": "/static/js/2.4fc7d10c.chunk.js.gz"
+  }
+];
